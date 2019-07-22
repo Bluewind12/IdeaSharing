7be095c7160e0ec77428a9dc476700a3956c9a32
bluewind_mac <zero18030@gmail.com>
2019-07-22 22:00:13 +0900

    Activityの作成（ADD）

---
+++ app/src/main/java/momonyan/ideasharing/DebugActivity.kt
@@ -0,0 +1,32 @@
+package momonyan.ideasharing
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.debug_layout.*
+
+class DebugActivity : AppCompatActivity() {
+    private val tag = "DEBUG"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.debug_layout)
+
+        val db = FirebaseFirestore.getInstance()
+
+        button.setOnClickListener {
+            val dbMap = HashMap<String, Any>()
+            dbMap["Title"] = titleEditText.text.toString()
+            dbMap["Contents"] = contentsEditText.text.toString()
+            db.collection("Data")
+                .add(dbMap)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(tag, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(tag, "Error adding document", e)
+                }
+        }
+
+    }
+}
