2436c256f5508ed2e66cd15168260cf19533add3
bluewind_mac <zero18030@gmail.com>
2019-08-06 23:59:30 +0900

    Tagクリック時の動作の追加

--- app/src/main/java/momonyan/ideasharing/adapter/TagListRecyclerAdapter.kt
+++ app/src/main/java/momonyan/ideasharing/adapter/TagListRecyclerAdapter.kt
@@ -1,10 +1,12 @@
 package momonyan.ideasharing.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import momonyan.ideasharing.R
+import momonyan.ideasharing.activity.DetailSearchActivity
 import momonyan.ideasharing.holder.TagListRecyclerHolder
 
 
@@ -14,6 +16,15 @@ class TagListRecyclerAdapter(private val context: Context, private val itemList:
     override fun onBindViewHolder(holder: TagListRecyclerHolder, position: Int) {
         holder.let {
             it.tagText.text = itemList[position]
+            it.tagCard.setOnClickListener {
+                val i = Intent(context, DetailSearchActivity::class.java)
+                i.putExtra("Title", "")
+                i.putExtra("Content", "")
+                i.putExtra("Tag", itemList[position])
+                i.putExtra("From", "-00:00:00")
+                i.putExtra("Since", "-23:59:59")
+                context.startActivity(i)
+            }
         }
     }
 
