8e9dbc29c303577f5ff23bbb0966e8bfb38551f9
bluewind_mac <zero18030@gmail.com>
2019-08-07 01:01:48 +0900

    ImageViewを丸くする

---
+++ app/src/main/java/momonyan/ideasharing/RoundRectCornerImageView.kt
@@ -0,0 +1,40 @@
+package momonyan.ideasharing
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.widget.ImageView
+
+
+class RoundRectCornerImageView : ImageView {
+
+    private val radius = 18.0f
+    private var path: Path? = null
+    private var rect: RectF? = null
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        init()
+    }
+
+    private fun init() {
+        path = Path()
+
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        rect = RectF(0f, 0f, this.width.toFloat(), this.height.toFloat())
+        path!!.addRoundRect(rect, radius, radius, Path.Direction.CW)
+        canvas.clipPath(path)
+        super.onDraw(canvas)
+    }
+}
